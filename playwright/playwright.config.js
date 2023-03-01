62ba94a587ebb0ffa27a4d1dd0933e40cdbc0edf
Lilla Vass <lilla@kubeshop.io>
2023-03-01 15:13:33 +0100

    feat: don't disable reporter

--- playwright/playwright.config.js
+++ playwright/playwright.config.js
@@ -33,7 +33,7 @@ const config = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html', { open: 'never' }]
+    ['html'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
