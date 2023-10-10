ccce9633ba7ce3ad9c192aea1d02819c0498d012
Lilla Vass <lilla@kubeshop.io>
2023-10-10 15:40:59 +0200

    new assertion

--- gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
+++ gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
@@ -8,8 +8,6 @@ class LibraryTest {
         String env = System.getenv("TESTKUBE_GRADLE");
         assertTrue(Boolean.parseBoolean(env), "TESTKUBE_GRADLE env should be true");
         String users = System.getProperty("users","/tmp/log");
-	System.out.println(users);
-        assertTrue(users == "500", users);
-        System.out.println(users);
+        assertTrue(users == "\$\{USERS\}", users);
     }
 }
