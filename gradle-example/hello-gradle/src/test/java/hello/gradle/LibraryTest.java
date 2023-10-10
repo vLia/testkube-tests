2f21c2b0a8c8ac6873fca419f3fe4e2870951c87
Lilla Vass <lilla@kubeshop.io>
2023-10-10 18:38:42 +0200

    java error

--- gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
+++ gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
     @Test void someLibraryMethodReturnsTrue() {
-        String users = System.getProperty("users","/tmp/log");
-        assertTrue(users == "set_users", "unexpected value for users");
+        String users = System.getProperty("users");
+        assertTrue(users.Equals("set_users"), "unexpected value for users");
     }
 }
