877ba65706a11d03fdf1eb8e93906716c84cc5c7
Lilla Vass <lilla@kubeshop.io>
2023-10-10 15:29:21 +0200

    use different variable name

--- gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
+++ gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
@@ -7,7 +7,7 @@ class LibraryTest {
     @Test void someLibraryMethodReturnsTrue() {
         String env = System.getenv("TESTKUBE_GRADLE");
         assertTrue(Boolean.parseBoolean(env), "TESTKUBE_GRADLE env should be true");
-        String log_dir = System.getProperty("users","/tmp/log");
+        String users = System.getProperty("users","/tmp/log");
 	System.out.println(users);
         assertTrue(users == "500", "users should be set to 500");
         System.out.println(users);
