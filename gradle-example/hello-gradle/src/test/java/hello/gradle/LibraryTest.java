8a1a0023e03f4c6be391f5794c0c2c2dfc49a6ad
Lilla Vass <lilla@kubeshop.io>
2023-10-10 18:47:52 +0200

    using vscode now

--- gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
+++ gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
-    @Test void someLibraryMethodReturnsTrue() {
+    @Test
+    void someLibraryMethodReturnsTrue() {
         String users = System.getProperty("users");
-        assertTrue(users.equals("set_users"), "unexpected value for users");
+        assertEquals("set_users", users, "unexpected value for users");
     }
 }
