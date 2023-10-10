9a8c802a3a85b37250f333a05448daabf90e4667
Lilla Vass <lilla@kubeshop.io>
2023-10-10 15:26:34 +0200

    getproperty

--- gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
+++ gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
@@ -7,8 +7,8 @@ class LibraryTest {
     @Test void someLibraryMethodReturnsTrue() {
         String env = System.getenv("TESTKUBE_GRADLE");
         assertTrue(Boolean.parseBoolean(env), "TESTKUBE_GRADLE env should be true");
-        String users = String.getString("users", 1); 
-        System.out.println(users);
+        String log_dir = System.getProperty("users","/tmp/log");
+	System.out.println(users);
         assertTrue(users == "500", "users should be set to 500");
         System.out.println(users);
     }
