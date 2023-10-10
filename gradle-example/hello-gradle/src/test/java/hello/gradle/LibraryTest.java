add90b2fc8fb847f6aa095a18595b7c81885b1d9
Lilla Vass <lilla@kubeshop.io>
2023-10-10 15:06:32 +0200

    add users

--- gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
+++ gradle-example/hello-gradle/src/test/java/hello/gradle/LibraryTest.java
@@ -8,6 +8,8 @@ class LibraryTest {
         String env = System.getenv("TESTKUBE_GRADLE");
         assertTrue(Boolean.parseBoolean(env), "TESTKUBE_GRADLE env should be true");
         String api = System.getenv("API_KEY");
+        int users = Integer.getInteger("users", 1); 
         assertTrue(api == "api_key_value", "API_KEY should have the value api_key_value");
+        assertTrue(users == 500, "users should be set to 500");
     }
 }
